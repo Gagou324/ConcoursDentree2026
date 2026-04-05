bb00b179c9456721d67dac471dd5ccdc1015c647
Gagou324 <benosmangabriel@gmail.com>
2026-04-05 15:16:30 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+const openBtn = document.getElementById("openModal");
+const closeBtn = document.getElementById("closeModal");
+const modal = document.getElementById("modal");
+
+openBtn.addEventListener("click", () => {
+    modal.classList.add("open");
+});
+
+closeBtn.addEventListener("click", () => {
+    modal.classList.remove("open");
+});
+
+const openBtn2 = document.getElementById("openModal2");
+const closeBtn2 = document.getElementById("closeModal2");
+const modal2 = document.getElementById("modal2");
+
+openBtn2.addEventListener("click", () => {
+    modal2.classList.add("open");
+});
+
+closeBtn2.addEventListener("click", () => {
+    modal2.classList.remove("open");
+});
+
+const openBtn3 = document.getElementById("openModal3");
+const closeBtn3 = document.getElementById("closeModal3");
+const modal3 = document.getElementById("modal3");
+
+openBtn3.addEventListener("click", () => {
+    modal3.classList.add("open");
+});
+
+closeBtn3.addEventListener("click", () => {
+    modal3.classList.remove("open");
+});
+
+const openBtn4 = document.getElementById("openModal4");
+const closeBtn4 = document.getElementById("closeModal4");
+const modal4 = document.getElementById("modal4");
+
+openBtn4.addEventListener("click", () => {
+    modal4.classList.add("open");
+});
+
+closeBtn4.addEventListener("click", () => {
+    modal4.classList.remove("open");
+});
+
+const openBtn5 = document.getElementById("openModal5");
+const closeBtn5 = document.getElementById("closeModal5");
+const modal5 = document.getElementById("modal5");
+
+openBtn5.addEventListener("click", () => {
+    modal5.classList.add("open");
+});
+
+closeBtn5.addEventListener("click", () => {
+    modal5.classList.remove("open");
+});
+
+const openBtn6 = document.getElementById("openModal6");
+const closeBtn6 = document.getElementById("closeModal6");
+const modal6 = document.getElementById("modal6");
+
+openBtn6.addEventListener("click", () => {
+    modal6.classList.add("open");
+});
+
+closeBtn6.addEventListener("click", () => {
+    modal6.classList.remove("open");
+});
